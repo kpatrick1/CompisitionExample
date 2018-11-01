8f208f29bc6b41310dfc5406a96ebdd976077b2d
Kevin <kpatrick@my.wctc.edu>
2018-10-31 19:11:14 -0500

    Initial commit

---
+++ src/com/company/Student.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Student {
+    private String name;
+    private int age;
+    private Address address;
+
+    public Student(String streetAddress, int zipCode, String state, String phone, String name, int age) {
+        //super(streetAddress, zipCode, state, phone);
+        address = new Address(streetAddress, zipCode, state, phone);
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getState() {
+        return address.getState();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
